33d700c0285f97fe3ca6257f174b167eb316c856
baaptiston <baba0661@gmail.com>
2025-02-11 14:07:23 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,13 +12,13 @@ endDate = dt.datetime.now()
 startDate = endDate - dt.timedelta(days = 365)
 
 data = yf.download(ticker, start = startDate, end = endDate)
-data["Returns"] = data.Close.pct_change()
+data_returns = data.Close
 
 st.subheader("Données historiques du titre")
 st.write(data.tail())
 
 st.subheader("Évolution du prix de clôture")
-fig = px.line(data, x=data.index, y=data["Close"], title=f"Prix de clôture de {ticker}")
+fig = px.line(data, x=data.index, y=data_returns, title=f"Prix de clôture de {ticker}")
 st.plotly_chart(fig)
 
 st.subheader("Distribution des rendements")
