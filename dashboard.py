f96183d0910dc4ff0734acde8d0a32e4746c2a87
baaptiston <baba0661@gmail.com>
2025-02-12 19:24:32 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -14,9 +14,6 @@ ticker = st.text_input('Entrez un ticker (ex : AAPL, TSLA, ^GSPC, ...)')
 end_date = dt.datetime.now()
 start_date = end_date - dt.timedelta(days = 365)
 data_ticker = yf.download(ticker, start = start_date, end = end_date)
-close_prices = pd.DataFrame(
-  data_ticker(data_ticker.index, data_ticker.Close),
-  columns = ['x', 'y'])
 
 # Premier graphique : affichage des données historiques du ticker
 st.subheader('Données historiques du titre')
